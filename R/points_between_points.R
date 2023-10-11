8d032efeba364456f55a7f5479774ce8748d7499
Greg McInerny <g.mcinerny@warwick.ac.uk>
2023-10-11 13:14:43 +0100

    Update points_between_points.R
    update for vertical lines which have infinite slope

--- R/points_between_points.R
+++ R/points_between_points.R
@@ -6,7 +6,7 @@
 #'
 #' @param point_1 coordinates of first point c( x1 ,y1 )
 #' @param point_2 coordinates of second point c( x2, y2 )
-#' @param distance value(s) for distances (proportion) of positions between points
+#' @param distance value(s) from 0 to 1 for distance(s) between points
 #'
 #' @return data frame of coordinates for the points, between points
 #' @export
@@ -29,15 +29,23 @@ points_between_points <- function( point_1, point_2, distance=0.5 ){
   a_slope <- y_diff / x_diff
 
   # calculate intercept from x and y of point 2
-  b_intercept <- point_2_a[2] - a_slope * point_2_a[1]
+  #if( a_slope != Inf ){
+  if( point_1_a[1] != point_2_a[1] ){
+      b_intercept <- point_2_a[2] - a_slope * point_2_a[1]
 
-  # get angle between points - not needed now
-  # xy_dist <- ( x_diff^2 + y_diff^2 )^0.5
-  # angle_between <- asin( y_diff / xy_dist )
+      # get angle between points - not needed now
+      # xy_dist <- ( x_diff^2 + y_diff^2 )^0.5
+      # angle_between <- asin( y_diff / xy_dist )
 
-  x_new_points <- point_1_a[1] + ( x_diff  * distance )
+      x_new_points <- point_1_a[1] + ( x_diff  * distance )
 
-  y_new_points <- a_slope * x_new_points + b_intercept
+      y_new_points <- a_slope * x_new_points + b_intercept
+  } else{
+      x_new_points <- rep( point_1_a[1], length( distance ) )
+      y_new_points <- seq( point_1_a[2] + y_diff * head( distance, n=1 ) ,
+                           point_1_a[2] + y_diff * tail( distance, n=1 ), l= length( distance ) )
+  }
+
 
   new_points <- as.data.frame( cbind( x_new_points, y_new_points ) )
 

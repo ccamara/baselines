ce0ff10a022cf98fca3accf46192ff7b4ea152b1
Greg McInerny <g.mcinerny@warwick.ac.uk>
2023-12-20 11:13:01 +0000

    New function for graphics device query

---
+++ R/graphics_which_device.R
@@ -0,0 +1,13 @@
+#' quick query to graphics device settings
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+graphics_which_device <- function( ){
+
+  current_graphics_device<-  options("device")
+  return( current_graphics_device)
+
+}
